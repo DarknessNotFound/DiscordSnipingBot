d91fabcf532e7c12ce57ff4e0f3a7228284fce29
DarknessNotFound <galbrightr@gmail.com>
2023-11-13 19:18:26 +0000

    Help commands added to players and snipes. Basic style cleanup

--- DiscordBot/cogs/AdminCommands.py
+++ DiscordBot/cogs/AdminCommands.py
@@ -7,6 +7,7 @@ from discord.ext import commands
 FILE_NAME = "AdminCommands"
 ADMIN_PERMISSION_LEVEL = 1
 
+
 def ExtractPlayerName(player: list) -> str:
     """Gets a playet's name based on the inputed player list.
 
@@ -18,11 +19,12 @@ def ExtractPlayerName(player: list) -> str:
     """
     if len(player) < 4:
         return ""
-    
+
     if player[1] != "":
         return f"<@{player[1]}>"
     else:
         return player[2]
+
 
 def SnipeToText(snipe: list) -> str:
     """Takes a given snipe list and converts it into printable discord text.
@@ -43,6 +45,7 @@ def SnipeToText(snipe: list) -> str:
 
     return f">>> **Id**: {snipe[0]}\n**Timestamp**: {snipe[1]}\n**Sniper**: {Sniper}\n**Sniped**: {Sniped}"
 
+
 class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -50,14 +53,14 @@ class Admin(commands.Cog):
 
     @commands.command(name='quit', help='')
     async def players(self, ctx, *args):
-        if DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL) == False:
+        if not DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL):
             await ctx.send("Action denied: Not high enough permission level.")
             return
         raise RuntimeError("User quit the program")
 
     @commands.command(name='AdminRules', help='')
     async def DisplayRules(self, ctx, *args):
-        if DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL) == False:
+        if not DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL):
             await ctx.send("Action denied: Not high enough permission level.")
             return
         msg = discord.Embed(
@@ -73,25 +76,35 @@ class Admin(commands.Cog):
         await ctx.send(embed=msg)
 
     # region Players
-    @commands.command(name='players', help='Various Player Options: -dis for discord id, -id for id, -n for name, -a for all, and -ad for all deleted')
+    @commands.command(name='players', help='Displays Players, use `>>players -h` for help')
     async def players(self, ctx, *args):
         """list players
 
         Args:
             ctx (_type_): _description_
-        """        
+        """
         try:
             Log.Command(ctx.author.id, "Players", ' '.join(args))
 
             if DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL) == False:
                 await ctx.send("Action denied: Not high enough permission level.")
                 return
-            
+
             if len(args) == 0:
-                await ctx.send("ERROR: no arguements given, use >>help players for help.")
+                await ctx.send("ERROR: no arguements given, use >>players -h or >>help players for help.")
                 return
-            
+
             PlayersToSend = []
+            if args[0] == "-h":
+                msg = "## Players Command Help\n"
+                msg += "* `-a` Gets all players\n"
+                msg += "* `-ad` Gets all deleted players\n"
+                msg += "* `-id [Player Id]` Gets a single player based on their id (must be an integer)\n"
+                msg += "* -`-dis [@PEOPLE]` Gets the discord Id\n"
+
+                await ctx.send(msg)
+                return
+
             if args[0] == "-dis" and len(args) > 1:
                 DiscordIds = DB.ExtractDiscordId(' '.join(args[1:]))
                 for discordId in DiscordIds:
@@ -100,12 +113,23 @@ class Admin(commands.Cog):
                         await ctx.send(f"<@{discordId}> does not exists in the database.")
                     else:
                         PlayersToSend.append(player)
-            
+
             if args[0] == '-a':
                 PlayersToSend = DB.ReadAllPlayers()
-            
+
             if args[0] == "-ad":
                 PlayersToSend = DB.ReadAllDeletedPlayers()
+
+            if args[0] == "-id":
+                if len(args) == 1:
+                    await ctx.send("Requires one id arguement, uses `>>players -h` for help")
+                    return
+
+                if not DB.PlayerExistsId(int(args[1])):
+                    await ctx.send(f"Player id '{args[1]}' not found in the database")
+                    return
+
+                PlayersToSend = [DB.ReadPlayerId(int(args[1]))]
 
             msg = discord.Embed(
                 title="Player(s)"
@@ -142,14 +166,14 @@ class Admin(commands.Cog):
     @commands.command(name='addplayer', help='Enter a player into the database. Put a list of @mentions for adding via discord id or just their name for a single individual (although this can not add multiple)')
     async def AddPlayer(self, ctx, *args):
         """Manually adds a player to the database.
-        """        
+        """
         try:
             Log.Command(ctx.author.id, "addplayer", ' '.join(args))
 
             if DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL) == False:
                 await ctx.send("Action denied: Not high enough permission level.")
                 return
-            
+
             if len(args) == 0:
                 await ctx.send(f"ERROR: {len(args)} arguements given but at least 1 is required, use **>>help addplayer** for help.")
                 return
@@ -178,7 +202,7 @@ class Admin(commands.Cog):
 
             count = 0
             for p in PlayersToSend:
-                if p[1] == "": #If discord id is empty
+                if p[1] == "":  # If discord id is empty
                     playerString = ">>> **Id**: {}\n**Discord Id**: {}\n**Permission Level**: {}".format(p[0], p[1], p[3])
                 else:
                     playerString = ">>> **Id**: {}\n**Discord Id**: <@{}>\n**Permission Level**: {}".format(p[0], p[1], p[3])
@@ -209,18 +233,16 @@ class Admin(commands.Cog):
             if DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL) == False:
                 await ctx.send("Action denied: Not high enough permission level.")
                 return
-            
-            await ctx.send("WIP: this command is still under construction.")
 
             if len(args) < 2:
                 await ctx.send(f"ERROR: {len(args)} arguements given but at least 2 are required, use **>>help renameplayer** for help.")
                 return
-            
+
             PlayerId = args[0]
-            if PlayerId.isdigit() == False:
+            if not PlayerId.isdigit():
                 await ctx.send(f"ERROR: Player Id arguement must be a number, \"{args[0]}\" given.")
                 return
-            
+
             if DB.PlayerExistsId(PlayerId):
                 Name = ' '.join(args[1:])
                 Updated = DB.UpdatePlayerName(PlayerId, Name=Name)
@@ -235,44 +257,58 @@ class Admin(commands.Cog):
             print(f"ERROR: In file \"{FILE_NAME}\" of command \"Players\"")
             print(f"Message: {str(ex)}")
             Log.Error(FILE_NAME, "RenamePlayer", str(ex))
-    #endregion
+    # endregion
 
-    #region Snipes
+    # region Snipes
 
-    @commands.command(name='snipes', help='Shows a list of all the snipes.')
+    @commands.command(name='snipes', help='Shows a list of all the snipes. Use `>>snipes -h` for help.')
     async def Snipes(self, ctx, *args):
         """Shows the snipes in the database with various flag options.
 
         Args:
-            ctx (_type_): 
-        """        
+            ctx (_type_):
+        """
         try:
             Log.Command(ctx.author.id, "snipes", ' '.join(args))
 
             if DB.AuthorHavePermission(ctx.author.id, ADMIN_PERMISSION_LEVEL) == False:
                 await ctx.send("Action denied: Not high enough permission level.")
                 return
-            
-            await ctx.send("WIP: this command is still under construction.")
 
             GroupOfSnipes = []
-            if len(args) == 0: # Most recent 5 snipes
+            if len(args) == 0:  # Most recent 5 snipes
                 GroupOfSnipes.append(("5 Most Recent Snipes", DB.ReadSnipes()))
-            elif args[0] == '-a': # All snipes.
+
+            elif args[0] == '-h':
+                msg = "## Snipes Command Option\n"
+                msg += "* `-h` gets the help command.\n"
+                msg += "* `-a` gets all snipes\n"
+                msg += "* `-pid [player id]` gets all the snipes of player. The ID must be an integer\n"
+                msg += "* `-did [Discord mentions]` gets all the snipes of all the players mentioned.\n"
+                msg += "* `-d [X]` Gets all the snipes from X days ago. X must be an integer.\n"
+                msg += "* `-dr [X] [Y]` Gets all the snipes from X to Y days ago. X, Y must be an integer. X, Y are inclusive.\n"
+                msg += "* `-sid [snipe id]` Gets a snipe based on its snipe id. Id must be an integer.\n"
+                msg += "* `-sidr [start snipe id] [end snipe id]` Gets all the snipes from a range of ids. Id range is inclusive and must be integers\n"
+                msg += "* `[X]` Gets the most recent X snipes. X must be an integer"
+
+                await ctx.send(msg)
+                return
+
+            elif args[0] == '-a':  # All snipes.
                 GroupOfSnipes.append(("All Snipes", DB.ReadAllSnipes()))
-            elif args[0] == '-pid': # All snipes related to a player's id.
+
+            elif args[0] == '-pid':  # All snipes related to a player's id.
                 if len(args) < 2:
                     await ctx.send("-pid requires 2 arguements, only 1 provided.")
-                elif args[1].isdigit() == False:
+                elif not args[1].isdigit():
                     await ctx.send("-pid requires an integer input for the player id.")
-                elif DB.PlayerExistsId(int(args[1])) == False:
+                elif not DB.PlayerExistsId(int(args[1])):
                     await ctx.send("Player not found.")
                 else:
                     GroupOfSnipes.append(("Confirmed Kills", DB.ReadSnipesOfSniper(int(args[1]))))
                     GroupOfSnipes.append(("Victim", DB.ReadSnipesOfSniped(int(args[1]))))
-            elif args[0] == '-did': # All snipes related to a player via @'ing them.
-                await ctx.send("WIP: this flag is still under construction.")
 
+            elif args[0] == '-did': # All snipes related to a player via @'ing them.
                 PlayersDiscordId = DB.ExtractDiscordId(''.join(args))
                 for DiscordId in PlayersDiscordId:
                     if DB.PlayerExistsDiscordId(DiscordId):
@@ -287,12 +323,22 @@ class Admin(commands.Cog):
 
             elif args[0] == '-d': # All snipes x days ago (default today).
                 await ctx.send("WIP: this flag is still under construction.")
-            elif args[0] == '-dr': # All snipes x to y days ago (default today or x to today).
+                days = 0
+
+                if len(args) == 2:
+                    days = 1
+                else:
+                    days = int(args[1])
+
+            elif args[0] == '-dr':  # All snipes x to y days ago (default today or x to today).
                 await ctx.send("WIP: this flag is still under construction.")
-            elif args[0] == '-sid': # Snipes via their id.
+
+            elif args[0] == '-sid':  # Snipes via their id.
                 await ctx.send("WIP: this flag is still under construction.")
-            elif args[0] == '-sidr': # Snipes with ids (inclusive) within range.
+
+            elif args[0] == '-sidr':  # Snipes with ids (inclusive) within range.
                 await ctx.send("WIP: this flag is still under construction.")
+
             else:
                 if args[0].isdigit():
                     GroupOfSnipes.append((f"{args[0]} Most Recent Snipes", DB.ReadSnipes(int(args[0]))))
@@ -325,7 +371,8 @@ class Admin(commands.Cog):
             
             if len(GroupOfSnipes) == 0:
                 await ctx.send("No snipes(s) found.")
-
+        except TypeError:
+            await ctx.send("Please have integer inputs")
         except Exception as ex:
             print(f"ERROR: In file \"{FILE_NAME}\" of command \"Snipes\"")
             print(f"Message: {str(ex)}")
@@ -425,7 +472,7 @@ class Admin(commands.Cog):
                 else:
                     MsgToSend = DB.UndoDeleteSnipe(id)
                 results.append((str(id), MsgToSend))
-            
+
             msg = discord.Embed(
                 title="Snipes Removal"
             )
@@ -469,9 +516,9 @@ class Admin(commands.Cog):
 
         except Exception as ex:
             Log.Error(FILE_NAME, "UpdateSnipe", str(ex))
-    #endregion
+    # endregion
 
-    #region Quotes
+    # region Quotes
 
     @commands.command(name='addquote', help='>>insertquote [QUOTE TEXT]')
     async def InsertQuote(self, ctx, *args):
@@ -491,7 +538,7 @@ class Admin(commands.Cog):
             if not DB.QualifiedQuote(' '.join(args)):
                 await ctx.send("Not a valid quote; the quote needs \"<a>\" and \"<v>\".")
                 return
-            
+
             QuoteId = DB.CreateQuote(Quote=' '.join(args))
             await ctx.send(f"Inserted new quote, id {QuoteId} into the database")
             return
